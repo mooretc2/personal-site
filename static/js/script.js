beb4b64811c00983d18ddcdf9a08c5495e702a14
mooretc2 <mooretc2@miamioh.edu>
2018-06-13 17:27:28 -0400

    changing fading to start at the middle instead of bottom

--- static/js/script.js
+++ static/js/script.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var objectMiddle = $(this).offset().top + ($(this).outerHeight()/2);
         
         /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if (objectMiddle < windowBottom) { //object comes into view (scrolling down)
           if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } else { //object goes out of view (scrolling up)
           if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
